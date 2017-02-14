f6c2d2a45bafed8e4f27523cdc5b74caaf31b615
Jakub Żuchowski <ellmunzai@gmail.com>
2017-02-14 12:25:37 +0100

    a simple authentication redirect GET action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,9 @@ class HomeController < ApplicationController
 
   def index; end
 
+  def auth_response
+    cookies[:github_client_id] = params[:code]
+    redirect_to root_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root "home#index"
+
+  get "auth_response", to: "home#auth_response"
 end
