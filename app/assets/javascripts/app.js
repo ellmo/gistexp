a73905d553e4004a393dceae140815d0e99ff9b1
Jakub Żuchowski <ellmunzai@gmail.com>
2017-02-14 12:26:23 +0100

    a simple oauth login

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,3 +1,13 @@
+const github_auth_url = "https://github.com/login/oauth/authorize?client_id="
+const github_client_id = "1921e4072d1b9108c5a8";
+
 const app = new Vue({
-  router
+  router,
+
+  methods: {
+    getAuth: function(){
+      window.location.href = github_auth_url + github_client_id;
+    },
+  },
 }).$mount('#app')
+
