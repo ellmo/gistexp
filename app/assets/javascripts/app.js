76e51c0410166acdd2b9af0dcb3263cc7877919f
Jakub Żuchowski <ellmunzai@gmail.com>
2017-02-14 13:58:56 +0100

    specified gist scope only

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,5 +1,6 @@
-const github_auth_url = "https://github.com/login/oauth/authorize?client_id="
-const github_client_id = "1921e4072d1b9108c5a8";
+const github_auth_url  = "https://github.com/login/oauth/authorize?";
+const github_client_id = "client_id=1921e4072d1b9108c5a8&";
+const github_scope     = "scope=gist"; // "repo,gist"
 
 const app = new Vue({
   router,
@@ -14,7 +15,7 @@ const app = new Vue({
   },
   methods: {
     getAuth: function(){
-      window.location.href = github_auth_url + github_client_id;
+      window.location.href = github_auth_url + github_client_id + github_scope;
     },
   },
 }).$mount('#app')
